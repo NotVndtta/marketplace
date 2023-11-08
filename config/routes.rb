c1f09654686490df22573cbcd2f77c16735ef3f6
NotVndtta <thenotalexey192@gmail.com>
2023-11-09 01:20:33 +0300

    Some style fixes edit button

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: 'users/registrations'}
   root "profiles#index"
-  get 'profiles/index'
   get "/profiles", to: "profiles#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
