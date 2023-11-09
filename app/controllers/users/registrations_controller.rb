008953f8a465664e65bd2dbfd2f03eeb1bcbc4a9
NotVndtta <thenotalexey192@gmail.com>
2023-11-10 01:04:34 +0300

    full authentication

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,8 +1,9 @@
 # frozen_string_literal: true
 
 class Users::RegistrationsController < Devise::RegistrationsController
-  # before_action :configure_sign_up_params, only: [:create]
-  # before_action :configure_account_update_params, only: [:update]
+ 
+  before_action :configure_sign_up_params, only: [:create]
+  before_action :configure_account_update_params, only: [:update]
 
   # GET /resource/sign_up
   # def new
@@ -42,12 +43,12 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_sign_up_params
-     devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+     devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :first_name])
    end
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_account_update_params
-     devise_parameter_sanitizer.permit(:account_update, keys: [:username])
+     devise_parameter_sanitizer.permit(:account_update, keys: [:username, :first_name, :last_name])
    end
 
   # The path used after sign up.
@@ -68,3 +69,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
     resource.update_without_password(params.except "current_password")
   end
 end
+
+
+  
+
