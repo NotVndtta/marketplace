cf90f953341309b0cd635f5c8938d39123fe5101
NotVndtta <thenotalexey192@gmail.com>
2023-12-15 16:54:20 +0300

    autorization fix

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,7 @@
 class ProductsController < ApplicationController
   before_action :set_product, only: %i[ show edit update destroy ]
   before_action :set_product, only: [:show, :edit, :update, :destroy] 
+  before_action :require_admin, only: [:edit, :update, :destroy]
   def index
     @products = Product.all
   end
@@ -21,9 +22,16 @@ class ProductsController < ApplicationController
   end
 
   def edit
+    authorize @product
+    if @product.edit(product_params)
+      redirect_to @product, notice: 'Product was successfully updated.'
+    else
+      render :edit
   end
+end
 
   def update
+    authorize @product
     if @product.update(product_params)
       redirect_to @product, notice: 'Product was successfully updated.'
     else
@@ -38,6 +46,12 @@ class ProductsController < ApplicationController
   private
   def set_product
     @product = Product.find(params[:id])
+  end
+end
+
+def require_admin
+  if current_user.role == 'user'
+    redirect_to root_path, alert: 'У вас нет прав для выполнения этого действия'
   end
 end
 
